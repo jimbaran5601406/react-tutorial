1d1e8d7ee6a2c9cdfc0575088a933e259b0720ee
KeiFunatsuya <jimbaran5601406@gmail.com>
2020-12-24 16:47:26 +0900

    途中
    https://ja.reactjs.org/tutorial/tutorial.html#why-immutability-is-importantから

--- src/index.js
+++ src/index.js
@@ -4,13 +4,36 @@ import "./index.css";
 
 class Square extends React.Component {
 	render() {
-		return <button className="square">{/* TODO */}</button>;
+		return (
+			<button className="square" onClick={() => this.props.onClick()}>
+				{this.props.value}
+			</button>
+		);
 	}
 }
 
 class Board extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			// squaresにnullを9個格納した配列を定義
+			squares: Array(9).fill(null)
+		};
+	}
+
+	handleClick(i) {
+		const squares = this.state.squares.slice();
+		squares[i] = "X";
+		this.setState({ squares: squares });
+	}
+
 	renderSquare(i) {
-		return <Square />;
+		return (
+			<Square
+				value={this.state.squares[i]}
+				onClick={() => this.handleClick(i)}
+			/>
+		);
 	}
 
 	render() {
